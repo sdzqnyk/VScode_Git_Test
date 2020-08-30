f9ead4d7ea0796f833962c5a83300f98cd6b55a5
sdzqnyk <540195127@qq.com>
2020-08-31 00:02:26 +0800

    第二次提交

---
+++ 1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: niuyukai
+email: 0906150215@csu.edu.cn
+Date: 2020-08-30 22:40:04
+LastEditTime: 2020-08-31 00:01:58
+Description: Modify here please
+FilePath: \VScode\1.py
+'''
+import numpy as np
+import gym
