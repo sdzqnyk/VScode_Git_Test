1ec3c423f6814690552561f1b1b0086a393fe20d
sdzqnyk <540195127@qq.com>
2020-08-31 08:29:31 +0800

    第二次提交

--- 1.py
+++ 1.py
@@ -4,9 +4,10 @@
 Author: niuyukai
 email: 0906150215@csu.edu.cn
 Date: 2020-08-30 22:40:04
-LastEditTime: 2020-08-31 00:01:58
+LastEditTime: 2020-08-31 08:28:56
 Description: Modify here please
 FilePath: \VScode\1.py
 '''
 import numpy as np
 import gym
+import matplotlib.pyplot as plt
